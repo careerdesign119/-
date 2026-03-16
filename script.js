d987648872eafc0a37c4cdab18a23a06360db07b
careerdesign119 <careerdesign119@gmail.com>
2026-03-16 16:13:49 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxPn-rQGn_-1pLMycdf4BwSG9BsHhKVFoIzTSz7CRilKnj-RMRrTl3PUsOcTwIhon0/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxVyLRyui6jvA76U9ovtuJRISFLHCVtAHXSLjYKJ6wD-0W_SMxT0r5nzQCkex8uhHAF/exec";
 
 // ===== 전역 상태 =====
 let state = { companies: [], teams: [], investments: [] };
